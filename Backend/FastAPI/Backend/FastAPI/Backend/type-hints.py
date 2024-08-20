0bef3245e797029886c3c7eb427e26d0f84a0878
mgodoydev <mathiasgodoydev@gmail.com>
2024-08-20 17:49:07 -0400

    Eliminando duplicados

--- Backend/FastAPI/Backend/FastAPI/Backend/type-hints.py
+++
@@ -1,8 +0,0 @@
-#Typehints
-my_string_variable = "My string variable"
-print(my_string_variable)
-print(type(my_string_variable))
-
-my_string_variable = 10
-print(my_string_variable)
-print(type(my_string_variable))
