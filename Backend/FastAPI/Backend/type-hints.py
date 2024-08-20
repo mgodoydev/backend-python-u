7da412053c2a9757eb14815c1bfdcea493f24d53
mgodoydev <mathiasgodoydev@gmail.com>
2024-08-20 16:28:14 -0400

    commit

---
+++ Backend/FastAPI/Backend/type-hints.py
@@ -0,0 +1,8 @@
+#Typehints
+my_string_variable = "My string variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 10
+print(my_string_variable)
+print(type(my_string_variable))
